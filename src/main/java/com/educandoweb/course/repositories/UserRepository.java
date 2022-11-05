876bfcab82ed907890c5c1b914640c48a6958e0c
Allas Assis <allasassis10@gmail.com>
2022-11-05 14:38:41 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course.entities.User;
 
+@Repository // @Repository não é necessário porque a classe já está herdanddo do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long>{ // repositories geralmente são interfaces
 	
 	
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 // esse pacote disponibiliza recursos web para a classe user
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	// RespondeEntity é um tipo especifico do Spring para retornar respostas de requisições web
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9492932", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		// User u = new User(1L, "Maria", "maria@gmail.com", "9492932", "12345");
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value= "/{id}") 				// isso indica que a requisição vai aceitar um ID dentro da URL
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }

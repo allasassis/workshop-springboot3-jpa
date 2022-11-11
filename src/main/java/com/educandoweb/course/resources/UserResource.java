59cfe91694dfdc2ea752a82ce5965f898dc547d0
Allas Assis <allasassis10@gmail.com>
2022-11-11 16:24:20 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -33,4 +37,14 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	// quando vamos inserir alguma coisa, usamos o POST e é por isso que usaremos o postmapping aqui
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) { //requestBody serve para que o json seja de deserializado para o arquivo java
+		obj = service.insert(obj);
+		// o padrão do HTTP é que, quando retornar um código 201, retornar também um cabeçalho com o novo endereço daquilo que você criou
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 }

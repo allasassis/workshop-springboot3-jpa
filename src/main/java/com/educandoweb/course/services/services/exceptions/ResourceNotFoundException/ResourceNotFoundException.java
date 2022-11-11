c54495c976eadcee1362e4788a494706fa132388
Allas Assis <allasassis10@gmail.com>
2022-11-11 17:09:42 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/services/services/exceptions/ResourceNotFoundException/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.services.services.exceptions.ResourceNotFoundException;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	//aqui estamos recebendo o ID que tentamos encontrar, mas não encontramos
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
